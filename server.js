93975e5b70d361a7d65603a602497fb45af61e3e
Bruce-jimenez <jimenezbruce2022@gmail.com>
2022-12-01 15:25:39 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 app.get('/greeting', (req, res) => res.send('Hello, stranger'));
 app.get('/greeting/:name', (req, res) => res.send('<h1>"What is up <name>. It is so great to see you."</h1>'));
 
+//Tips
+app.get('/tip/:total/:percent', (req, res) => {
+    res.send(`<h2> Your total is ${req.params.total}. Your tip should be ${req.params.total * (req.params.percent /100)}. `)
+}).
+
+
+
 
 
 app.listen(3000, () => {
